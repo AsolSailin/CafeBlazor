238aca243728332afee0102b257f3e97f836f751
Ruslana <garaeva-r_23@mail.ru>
2023-12-05 14:12:20 +0300

    Adding CurrentElementService

--- CafeBlazor/CafeBlazor/Services/CurrentElementService.cs
+++ CafeBlazor/CafeBlazor/Services/CurrentElementService.cs
@@ -1,6 +1,11 @@
-﻿namespace CafeBlazor.Services
+﻿using CafeBlazor.DataBase;
+
+namespace CafeBlazor.Services
 {
     public class CurrentElementService
     {
+        public Account? CurrentAccount { get; set; }
+        public User? CurrentUser { get; set; }
+        public string? CurrentRole { get; set; }
     }
 }

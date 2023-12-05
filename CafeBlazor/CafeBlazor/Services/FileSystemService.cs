4df45d23ea586c583886d0ff9a6263948efb0e18
Ruslana <garaeva-r_23@mail.ru>
2023-12-05 14:10:46 +0300

    Adding FileSystemService

--- CafeBlazor/CafeBlazor/Services/FileSystemService.cs
+++ CafeBlazor/CafeBlazor/Services/FileSystemService.cs
@@ -2,5 +2,23 @@
 {
     public class FileSystemService
     {
+        string path = "";
+
+        public async Task UploadFileAsync(string name, Stream stream, string animalOrEmployee)
+        {
+
+            switch (animalOrEmployee)
+            {
+                case "meal":
+                    path = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images\meal", name);
+                    break;
+                case "user":
+                    path = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images\users", name);
+                    break;
+            }
+
+            using var fs = new FileStream(path, FileMode.Create);
+            await stream.CopyToAsync(fs);
+        }
     }
 }
